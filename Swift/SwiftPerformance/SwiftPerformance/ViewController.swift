277ee7b9373b6894fc6c9b49d9d420a2e54b12f9
Johyeon Yoon <jalynneyoon@gmail.com>
2024-11-26 12:23:16 +0900

    Initialize SwiftPerformance app

---
+++ Swift/SwiftPerformance/SwiftPerformance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPerformance
+//
+//  Created by 윤조현 on 11/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift/SwiftPerformance/SwiftPerformanceTests/SwiftPerformanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPerformanceTests.swift
+//  SwiftPerformanceTests
+//
+//  Created by 윤조현 on 11/26/24.
+//
+
+import Testing
+@testable import SwiftPerformance
+
+struct SwiftPerformanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
